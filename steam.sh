8eb0efe64e32b69e26510014383978a29420e40d
Timo Schrappe <timo@thirdman.de>
2014-09-13 17:07:22 +0200

    Inital commit. Specified some features at the top of the steam.sh

---
+++ steam.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO:
+# - Install server in current directory or by a given directory (--dir=/home/steam/csgo01)
+# - Start, Stop, Restart, Status the server in the current directory or in the given directory
+# - Save pidfile with -pidfile filename.pid (stored in ~/csgo/)
+# - Connect into tmux session
+# - Download latest steamcmd before installing gameserver
+# - Renice Server process
